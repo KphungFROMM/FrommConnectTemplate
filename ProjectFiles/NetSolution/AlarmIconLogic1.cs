43741afcb9995ab724fe74573e4141c3f55f09d1
KphungFROMM <kphung@fromm.com>
2024-05-22 13:31:56 -0400

    Adding Template Project

---
+++ ProjectFiles/NetSolution/AlarmIconLogic1.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.RAEtherNetIP;
+using FTOptix.OPCUAServer;
+#endregion
+
+public class AlarmIconLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
